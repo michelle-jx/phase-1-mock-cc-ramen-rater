750a670337a132b4aa861dad85b1dec688b04dba
Michelle Too <mjxtcodes@gmail.com>
2022-09-28 09:53:03 -0700

    submission #1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
 // write your code here
+let addRamen = false;
+
+const url = 'http://localhost:3000';
+
+const addNewRamen = () => {
+    const addBtn = document.querySelector('#new-ramen-button');
+    const ramenForm = document.querySelector('.container')
+    addBtn.addEventListener("click", () => {
+        addRamen = !addRamen;
+        if (addRamen) {
+            ramenForm.style.display = "block";
+        } else {
+            ramenForm.style.display = "none";
+        }
+    });
+}
+
+const fetchImages = () => {
+    fetch(url)
+        .then(resp => resp.json())
+        .then(json => {
+            makeRamenPhotos(json)
+            console.log(json)
+        })
+}
+
+const renderRamenImages = (ramens) => {
+    const ramenDiv = document.getElementById('ramen-menu')
+    ramens.forEach(ramen => {
+        const ramenCard = makeRamenCard(ramen);
+    })
+}
+
+const listenForFormSubmit = () => {
+    const form = document.querySelector('ramen-detail')
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        const ramenName = e.target[0].value;
+        const ramenRestaurantName = e.target[1].value;
+
+        const newRamen = {
+            name: ramens.name,
+            restaurantName: ramens.restaurant,
+            image: ramens.image,
+            rating: ramens.rating,
+            comment: ramens.comment,
+        };
+    })
+}
+
+const makeRamenCard = ramen => {
+   const ramenCard = document.createElement('div')
+   const ramenName = document.createElement('h2')
+   const ramenImage = document.createElement('img')
+   const ramenRating = document.createElement('rating-display')
+   const ramenComment = document.createElement('comment-display')
+
+   ramenCard.className = 'card'
+   ramenImage.className = 'ramen-image'
+   ramenName.textContent = ramen.name,
+   ramenRestaurant.textContent = ramen.restaurantName,
+   ramenImage.src = ramen.image,
+   ramenRating.textContent = ramen.rating,
+   ramenComment.textContent = ramen.comment
+
+   ramenCard.append(ramenName, ramenRestaurantName, ramenRating, ramenComment);
+   const ramenCollection = document.getElementById('ramen-menu')
+   ramenCollection.append(ramenCard)
+}   
